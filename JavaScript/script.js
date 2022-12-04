25dac9586cb6fc4e81875343905873b899a24fa5
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-04 12:01:33 +0530

    click count completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,5 +3,7 @@ var clickCount = document.getElementById('click-count');
 count = 0;
 
 box.addEventListener('click', function(){
-    
+    count++;
+    console.log(count);
+    clickCount.innerText = count + " ";
 })
